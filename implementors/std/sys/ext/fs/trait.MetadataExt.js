b5d53d4cca1f819d2410e41e001955cfce3df9f8
Håvar Nøvik <havar@ravn.no>
2016-05-16 17:52:25 +0200

    Update docs to 0.3.0.

--- implementors/std/sys/ext/fs/trait.MetadataExt.js
+++ implementors/std/sys/ext/fs/trait.MetadataExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['multimap'] = [];
+implementors['multimap'] = [];implementors['multimap'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
